be3e45513e9553d02f7237f94c56d2fc3e075b8d
ValeriaQuimi <vdquimi@espol.edu.ec>
2025-08-11 17:28:12 -0500

    Primer Pantalla

---
+++ app/src/main/java/com/example/pro/PrimerPantalla.java
@@ -0,0 +1,35 @@
+package com.example.pro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PrimerPantalla extends AppCompatActivity {
+
+    private Button btn_Ingresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_primer_pantalla);
+
+        btn_Ingresar= findViewById(R.id.btn_ingresar);
+
+        btn_Ingresar.setOnClickListener(this::ingresar);
+    }
+
+    public void ingresar(View view){
+
+        //Mostrar la activity de mapa
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
